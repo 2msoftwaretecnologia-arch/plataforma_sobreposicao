b7b537ca2f78861b67b65c411c0d61d150aacb56
Leonardo Capistrano <leonardo.capistrano445@gmail.com>
2025-11-19 20:58:32 -0300

    Adopt minimal monochrome layout

--- analysis/urls.py
+++ analysis/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UploadZipCarView.as_view(), name='homepage'),
+    path('', views.AnswerspageView.as_view(), name='homepage'),
     path('upload/', views.UploadZipCarView.as_view(), name='upload_zip_car'),
 ]
