542875848eb080567312ee8b7895dedfbf8d0e94
LCapistrano25 <leonardo.capistrano445@gmail.com>
2025-12-01 19:28:39 -0300

    fix(settings): add localhost and service hosts to ALLOWED_HOSTS
    Enable local development and service communication by including additional host entries

--- kernel/settings.py
+++ kernel/settings.py
@@ -26,7 +26,13 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['69.62.126.40']
+ALLOWED_HOSTS = [
+    "69.62.126.40",
+    "localhost",
+    "127.0.0.1",
+    "overlay-app",
+    "nginx"
+]
 
 # Application definition
 
