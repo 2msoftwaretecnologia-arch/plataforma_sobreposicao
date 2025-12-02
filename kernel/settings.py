db83bbda54d238b602ec069a4e5b082142f2b4db
LCapistrano25 <leonardo.capistrano445@gmail.com>
2025-12-02 17:16:23 -0300

    refactor(settings): remove unused staticfiles directory configuration
    The STATICFILES_DIRS setting is no longer needed as the static files are now handled differently in the project structure.

--- kernel/settings.py
+++ kernel/settings.py
@@ -152,9 +152,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "analysis" / "static",
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
