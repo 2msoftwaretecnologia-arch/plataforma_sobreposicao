90603ed10c4f4bdb608ca68782acbbb58f15e971
LCapistrano25 <leonardo.capistrano445@gmail.com>
2025-12-02 17:36:50 -0300

    refactor(settings): update static and media file paths to use pathlib
    Use pathlib's Path object for better path handling consistency across the project

--- kernel/settings.py
+++ kernel/settings.py
@@ -152,8 +152,12 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "analysis" / "static",
+]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Authentication redirects
 LOGIN_URL = '/accounts/login/'
